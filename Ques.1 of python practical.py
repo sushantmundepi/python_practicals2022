57bed380cf6da7c350e767f600ff0adac59bae61
sushantmundepi <100505085+sushantmundepi@users.noreply.github.com>
2023-02-16 22:47:25 +0530

    Update Ques.1 of python practical.py

--- Ques.1 of python practical.py
+++ Ques.1 of python practical.py
@@ -9,14 +9,14 @@ while True :
     #for real roots
     if d>0 :
         d = (b**2) - (4*a*c)
-
         # find the two real solutions
-        sol1 = (-b-cmath.sqrt(d))/(2*a)
-        sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-        print('The solution are {0} and {1}'.format(sol1,sol2))
-
-    elif d<0 :
+        r1 = (-b-cmath.sqrt(d))/(2*a)
+        r2 = (-b+cmath.sqrt(d))/(2*a)
+        print('The solution are' , (r1,r2))
+        break
+    elif d==0 :
+        r=-b/2*a
+        print("both the roots are same" , r)
+    else :
         print("no real roots")
         break
-
